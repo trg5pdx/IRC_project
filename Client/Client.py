00132ffebed3b23b732ad42e5c5ff39fe3a9cfec
Thomas <trg5@pdx.edu>
2022-10-26 18:48:44 -0700

    Threw together a basic server client to see messages being passed between the two

--- Client/Client.py
+++ Client/Client.py
@@ -1,1 +1,21 @@
-# Initial commit
+# Made by Thomas Gardner, 2022
+# Using the TCP client example from the book to get set up
+
+from socket import *
+import sys
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+serverPort = 45876 
+serverName = 'localhost'
+clientSocket.connect((serverName, serverPort))
+
+message = "" 
+
+while message != "/quit":
+    message = input("Enter your message:")
+    clientSocket.send(message.encode())
+    response = clientSocket.recv(1024).decode()
+    print(response)
+
+clientSocket.close()
+sys.exit()
--- Server/Server.py
+++ Server/Server.py
@@ -1,1 +1,28 @@
-# initial commit
+# Made by Thomas Gardner, 2022
+# Using the HTTP server code from the books coding problems to help with
+# starting the server code
+
+from socket import *
+import sys
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+serverPort = 45876 
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+
+print("Ready to accept connections")
+connectionSocket, addr = serverSocket.accept()
+message = ""
+
+while message != "/quit":
+    message = connectionSocket.recv(1024).decode()
+    print(message)
+    sentence = input("Enter your response:")
+    connectionSocket.send(sentence.encode())
+
+if message == "/quit":
+    connectionSocket.send("Connection shutting off...".encode())
+
+serverSocket.close()
+sys.exit()

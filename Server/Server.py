5bba21e03d21877f3688d40195136b67f980ddb8
Thomas <trg5@pdx.edu>
2022-11-06 23:07:08 -0800

    Added a prompt on the server side for commands, and set up a proper main function; need to clean up and rename further

--- Server/Server.py
+++ Server/Server.py
@@ -18,32 +18,62 @@ lock = threading.Condition()
 def handler(client_socket):
     print("ping")
     response = ""
-    while response != "/quit":
-        if len(queue) > 0:
-            for i in queue:
-                i = i + "\n"
-                client_socket.send(i.encode())
-        response = client_socket.recv(1024).decode()
-        print(response)
-        lock.acquire()
-        print("lock acquired")
-        queue.append(response)
-        client_socket.send("message received".encode())
-        if response == "/quit":
-            client_socket.send("disconnecting...".encode())
-            client_socket.close()
-        lock.release()
-        print("lock freed")
-        print(queue)
+    with client_socket:
+        while response != "/quit":
+            if len(queue) > 0:
+                for i in queue:
+                    i = i + "\n"
+                    client_socket.send(i.encode())
+            response = client_socket.recv(1024).decode()
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
+            if response == "/quit":
+                client_socket.send("disconnecting...".encode())
+                client_socket.close()
+            else:
+                lock.acquire()
+                print(response)
+                print("lock acquired")
+                queue.append(response)
+                client_socket.send("message received".encode())
+                lock.release()
+                print("lock freed")
 
-serverPort = 45876 
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
+def accept_connections(server_socket):
+    while True:
+        connection_socket, addr = server_socket.accept()
+        threading.Thread(target=handler, args=(connection_socket,), daemon=True).start() 
 
-print("Ready to accept connections\n")
+def print_help():
+    print("/help: prints this menu")
+    print("/history: prints out the current message history")
+    print("/quit: closes the server")
 
-while True:
-    connectionSocket, addr = serverSocket.accept()
-    threading.Thread(target=handler, args=(connectionSocket,), daemon=True).start() 
+def main():
+    server_socket = socket(AF_INET, SOCK_STREAM)
+
+    server_port = 45876 
+    server_host = 'localhost'
+    server_socket.bind((server_host, server_port))
+    server_socket.listen(1)
+
+    print("Ready to accept connections\n")
+    # Spinning up another thread to allow for it to accept connections and have the
+    # server be able to run commands on it's own
+    threading.Thread(target=accept_connections, args=(server_socket,), daemon=True).start()
+    
+    server_command = ""
+
+    while server_command != "/quit":
+        server_command = input()
+        match server_command:
+            case "/help":
+                print_help()
+            case "/history":
+                print(queue)
+            case "/quit":
+                print("closing server...")
+            case other:
+                print("Unknown command")
+
+if __name__ == '__main__':
+    main()
